c49086e00021ad8eb89c7fdb4ddd675e9fb33b8a
Rango <rango.yuan@goldmf.com>
2016-09-27 15:13:12 +0800

    react-route go

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router'
-import {Home, Article, Login} from './components'
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
+import { Home, Article, Login } from './components'
 
-const App = React.createClass({
-    render:()=>(
-        <Login />
-    )
-})
 const About = React.createClass({
     render:()=>(
-        <Aritcle />
+        <Article />
     )
 })
 const NoMatch = React.createClass({
@@ -19,56 +14,30 @@ const NoMatch = React.createClass({
     )
 })
 
-const Users = React.createClass({
+const App = React.createClass({
   render() {
     return (
       <div>
-        <h1>Users</h1>
-        <div className="master">
-          <ul>
-            {/* use Link to route around the app */}
-            {this.state.users.map(user => (
-              <li key={user.id}><Link to={`/user/${user.id}`}>{user.name}</Link></li>
-            ))}
-          </ul>
-        </div>
-        <div className="detail">
-          {this.props.children}
-        </div>
+        <h1>App</h1>
+        {/* change the <a>s to <Link>s */}
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/login">login</Link></li>
+        </ul>
+        {this.props.children}
       </div>
     )
   }
 })
 
-const User = React.createClass({
-  componentDidMount() {
-    this.setState({
-      // route components are rendered with useful information, like URL params
-      user: findUserById(this.props.params.userId)
-    })
-  },
+const routes = {
+  path: '/',
+  component: App,
+  indexRoute: { component: Home },
+  childRoutes: [
+    { path: 'about', component: About },
+    { path: 'login', component: Login },
+  ]
+}
 
-  render() {
-    return (
-      <div>
-        <h2>{this.state.user.name}</h2>
-        {/* etc. */}
-      </div>
-    )
-  }
-})
-
-// Declarative route configuration (could also load this config lazily
-// instead, all you really need is a single root route, you don't need to
-// colocate the entire config).
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="about" component={About}/>
-      <Route path="users" component={Users}>
-        <Route path="/user/:userId" component={User}/>
-      </Route>
-      <Route path="*" component={NoMatch}/>
-    </Route>
-  </Router>
-), document.getElementById('root'))
+render(<Router history={hashHistory} routes={routes} />, document.getElementById('root'))
